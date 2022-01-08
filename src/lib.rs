f77ea1549042f195fbcaae1dfc00656f3d51347a
Matt Stetter <mjs2617@rit.edu>
2022-01-07 21:46:20 -0500

    Fixed text parsing and working on pick show

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,31 @@
-use std::fs;
+
 
 pub mod list_io {
-    pub fn get_list(file_name: &str) -> Vec<&str> {
+    pub fn get_list(file_name: &str) -> Result<Vec<String>, std::io::Error> {
+        let file_text = match std::fs::read_to_string(file_name) {
+            Ok(text) => text,
+            Err(e) => return Err(e),
+        };
 
-        let contents = std::fs::read_to_string(file_name)
-            .expect("Could not find specified file");
+        let mut list: Vec<String> = Vec::new();
+        for title in file_text.lines() {
+            list.push(String::from(title));
+        }
+        Ok(list)
+    }
+}
 
-        let mut titles: Vec<&str> = Vec::new();
-        
-        vec!["1, 2, 3"]
+#[cfg(test)]
+mod tests {
+    use super::list_io;
+    #[test]
+    fn correct_file_contents() {
+        assert_eq!(vec!["eawgf", "hgre", "ghfesd", "ds"],
+         list_io::get_list("test.txt").unwrap());
+    }
+
+    #[test]
+    fn nonexistant_file() {
+        assert!(list_io::get_list("random").is_err());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 use std::env;
+use rand::seq::SliceRandom;
+
+use picker::list_io;
+
+const FILENAME: &str = "anime_list.txt";
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -6,5 +11,16 @@ fn main() {
 
     if num_args == 1 {
         panic!("Error: insufficient arguments");
+    }
+
+    let mut titles = list_io::get_list(FILENAME).unwrap();
+    let choice: &str;
+
+    if let "pick" = &(args[1].to_lowercase())[..] {
+        choice = match titles.choose(&mut rand::thread_rng()) {
+            Some(c) => c,
+            None => { panic!("List cannot be empty"); }
+        };
+        println!("Watch next: {:?}", choice);
     }
 }
